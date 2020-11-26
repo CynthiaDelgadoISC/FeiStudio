8e1b8a53d5aad1a0cf953053540e4f15014f6b94
Alberto Lopez <betomper@gmail.com>
2020-11-26 13:44:14 -0600

    listo primeros 3 filtros de beto

--- app/src/main/java/com/example/feistudio/EdicionActivity.kt
+++ app/src/main/java/com/example/feistudio/EdicionActivity.kt
@@ -3,7 +3,6 @@ package com.example.feistudio
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Color.argb
 import android.net.Uri
@@ -34,7 +33,7 @@ class EdicionActivity:Activity() {
     private var finalBitmap: Bitmap? = null
     private var originalBitmap: Bitmap? = null
 
-    private var opcFlag: Boolean = false
+    private var separacionColoresCount: Int? = null
 
 
 
@@ -46,6 +45,7 @@ class EdicionActivity:Activity() {
         txtValor=findViewById(R.id.lblPorcentaje)
         btnGuardar = findViewById(R.id.btnGuardar)
         btnRevertir = findViewById(R.id.btnRevertir)
+
 
         skBar.isEnabled=false
         var opcion:String="brightness"
@@ -61,11 +61,7 @@ class EdicionActivity:Activity() {
             originalBitmap = data2.extras?.get("data") as Bitmap
             imgFoto.setImageBitmap(originalBitmap)
         }
-
-
-
-
-
+        var auxBitmap: Bitmap = originalBitmap!!
 
         recView = findViewById(R.id.recViewFiltros)
 
@@ -73,6 +69,9 @@ class EdicionActivity:Activity() {
                 Filtro("Negativo"),
                 Filtro("Escala Grises"),
                 Filtro("Brillo"),
+                Filtro("Contraste"),
+                Filtro("Gamma"),
+                Filtro("Separacin de Colores"),
                 Filtro("Gaussian Blur"),
                 Filtro("Embossing"),
                 Filtro("Sharpen"),
@@ -89,69 +88,104 @@ class EdicionActivity:Activity() {
                     Toast.LENGTH_SHORT).show()
             // convertir foto
             when(it.nombre){
-                "Blanco y Negro" ->{
+                "Blanco y Negro" -> {
                     finalBitmap = black_withe((imgFoto.drawable.toBitmap()))
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Negativo"->{
+                "Negativo" -> {
                     finalBitmap = invertirNegativo(imgFoto.drawable.toBitmap())
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Escala Grises" ->{
+                "Escala Grises" -> {
                     finalBitmap = grayScale(imgFoto.drawable.toBitmap())
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Brillo"->{
-                    opcion="brightness"
-                    skBar.isEnabled=true
-                    skBar.progress=100
+                "Brillo" -> {
+                    opcion = "brightness"
+                    skBar.isEnabled = true
+                    skBar.progress = 100
                 }
-                "Hue"->{
-                    opcion="applyHueFilter"
-                    skBar.isEnabled=true
-                    skBar.progress=100
+                "Contraste" -> {
+                    opcion = "contrast"
+                    skBar.isEnabled = true
+                    skBar.progress = 0
                 }
-                "Sepian" ->{
+                "Gamma" -> {
+                    finalBitmap = gamma(imgFoto.drawable.toBitmap(), 1.8,1.8,1.8)
+                    imgFoto.setImageBitmap(finalBitmap)
+                    skBar.isEnabled = false
+                    skBar.progress = 100
+                }
+                "Separacin de Colores" -> {
+                    if(separacionColoresCount == null){
+                        auxBitmap = imgFoto.drawable.toBitmap()
+                        separacionColoresCount = 0
+                    }
+                    when(separacionColoresCount){
+                        0 -> {
+                            finalBitmap = colorFilter(auxBitmap, 100.0,1.0,1.0)
+                            separacionColoresCount = separacionColoresCount!! + 1
+                        }
+                        1 -> {
+                            finalBitmap = colorFilter(auxBitmap, 1.0,100.0,1.0)
+                            separacionColoresCount = separacionColoresCount!! + 1
+                        }
+                        2 -> {
+                            finalBitmap = colorFilter(auxBitmap, 1.0,1.0,100.0)
+                            separacionColoresCount = 0
+                        }
+                    }
+                    imgFoto.setImageBitmap(finalBitmap)
+                    skBar.isEnabled = false
+                    skBar.progress = 100
+
+                }
+                "Hue" -> {
+                    opcion = "applyHueFilter"
+                    skBar.isEnabled = true
+                    skBar.progress = 100
+                }
+                "Sepian" -> {
                     finalBitmap = sepian(imgFoto.drawable.toBitmap())
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Espejo" ->{
+                "Espejo" -> {
                     finalBitmap = espejo(imgFoto.drawable.toBitmap())
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Wave" ->{
+                "Wave" -> {
                     finalBitmap = wave(imgFoto.drawable.toBitmap())
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Sharpen"->{
-                    finalBitmap = sharpen(imgFoto.drawable.toBitmap(),11.0)
+                "Sharpen" -> {
+                    finalBitmap = sharpen(imgFoto.drawable.toBitmap(), 11.0)
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Gaussian Blur" ->{
+                "Gaussian Blur" -> {
                     finalBitmap = gaussianBlur(imgFoto.drawable.toBitmap())
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
-                "Smoothing" ->{
-                    finalBitmap = smooth(imgFoto.drawable.toBitmap(),5.0)
+                "Smoothing" -> {
+                    finalBitmap = smooth(imgFoto.drawable.toBitmap(), 5.0)
                     imgFoto.setImageBitmap(finalBitmap)
-                    skBar.isEnabled=false
-                    skBar.progress=100
+                    skBar.isEnabled = false
+                    skBar.progress = 100
                 }
             }
 
@@ -168,16 +202,18 @@ class EdicionActivity:Activity() {
                     //hace un llamado a la perilla cuando se arrastra
                     override fun onProgressChanged(seekBar: SeekBar,
                                                    progress: Int, fromUser: Boolean) {
-                        txtValor.text=(progress-100).toString()
+                        txtValor.text = (progress - 100).toString()
 
                     }
+
                     //hace un llamado  cuando se toca la perilla
                     override fun onStartTrackingTouch(seekBar: SeekBar) {}
+
                     //hace un llamado  cuando se detiene la perilla
                     override fun onStopTrackingTouch(seekBar: SeekBar) {
-                        val valor=""+txtValor.text
-                        when(opcion) {
-                            "brightness"-> {
+                        val valor = "" + txtValor.text
+                        when (opcion) {
+                            "brightness" -> {
                                 finalBitmap = brightness(imgFoto.drawable.toBitmap(), valor.toInt())
                                 imgFoto.setImageBitmap(finalBitmap)
                             }
@@ -185,8 +221,12 @@ class EdicionActivity:Activity() {
                                 finalBitmap = applyHueFilter(imgFoto.drawable.toBitmap(), valor.toInt())
                                 imgFoto.setImageBitmap(finalBitmap)
                             }
+                            "contrast" -> {
+                                finalBitmap = contrast(imgFoto.drawable.toBitmap(), (valor).toInt() + 100)
+                                imgFoto.setImageBitmap(finalBitmap)
+                            }
                         }
-                        skBar.progress=100
+                        skBar.progress = 100
                     }
                 })
 
@@ -201,7 +241,7 @@ class EdicionActivity:Activity() {
                 null
             }
             if(photoFile != null){
-                MediaStore.Images.Media.insertImage(this.contentResolver, finalBitmap, currentPhotoPath,"FeiStudio")
+                MediaStore.Images.Media.insertImage(this.contentResolver, finalBitmap, currentPhotoPath, "FeiStudio")
                 Toast.makeText(this, "Imagen guardada!", Toast.LENGTH_SHORT).show()
             }
         }
@@ -226,7 +266,7 @@ class EdicionActivity:Activity() {
         }
     }
 
-    fun invertirNegativo(src:Bitmap): Bitmap{
+    fun invertirNegativo(src: Bitmap): Bitmap{
         val bmOut = Bitmap.createBitmap(src.width, src.height, src.config)
         // color info
         var A: Int
@@ -255,7 +295,7 @@ class EdicionActivity:Activity() {
         // Retorna el bitmap final
         return bmOut
     }
-    fun grayScale(src:Bitmap): Bitmap{
+    fun grayScale(src: Bitmap): Bitmap{
         val bmOut = Bitmap.createBitmap(src.width, src.height, src.config)
         // color info
         var A: Int
@@ -307,7 +347,7 @@ class EdicionActivity:Activity() {
         // Retorna el bitmap final
         return bmOut
     }
-    fun brightness(src: Bitmap, value:Int):Bitmap{
+    fun brightness(src: Bitmap, value: Int):Bitmap{
         val bmOut = Bitmap.createBitmap(src.width, src.height, src.config)
         // color info
         var A: Int
@@ -346,6 +386,109 @@ class EdicionActivity:Activity() {
         // Retorna el bitmap final
         return bmOut
     }
+    fun contrast(source: Bitmap, value: Int): Bitmap{
+        // size
+        if(value < 0){
+            return source
+        }
+        val width = source.width
+        val height = source.height
+        val bmpOut = Bitmap.createBitmap(width, height, source.config)
+        var A: Int; var R: Int; var G: Int; var B: Int;
+        var pixel: Int
+        val contrast = Math.pow(((100 + value) / 100f).toDouble(), 2.toDouble())
+        println("valor: ${value}")
+        println("contraste: ${contrast}")
+        for(y in 0 until height){
+            for(x in 0 until width){
+                // obtenemos pixel
+                pixel = source.getPixel(x, y)
+                A = Color.alpha(pixel)
+                // aplicamos el filtro solo a los canaes RGB
+                R = Color.red(pixel)
+                R = (((((R / 255f) - 0.5) * contrast) + 0.5) * 255f).toInt()
+                if(R < 0)
+                    R = 0
+                else if(R > 255)
+                    R = 255
+                G = Color.green(pixel)
+                G = (((((G / 255f) - 0.5) * contrast) + 0.5) * 255f).toInt()
+                if(G < 0)
+                    G = 0
+                else if(G > 255)
+                    G = 255
+                B = Color.blue(pixel)
+                B = (((((B / 255f) - 0.5) * contrast) + 0.5) * 255f).toInt()
+                if(B < 0)
+                    B = 0
+                else if(B > 255)
+                    B = 255
+                // asignamos el pixel modificado al nuevo bitmap
+                bmpOut.setPixel(x, y, Color.argb(A, R, G, B))
+            }
+        }
+
+        return bmpOut
+    }
+    fun gamma(src: Bitmap, red: Double, green: Double, blue: Double): Bitmap{
+        val width = src.width
+        val height = src.height
+
+        val bmpOut = Bitmap.createBitmap(width, height, src.config)
+        var A: Int; var R: Int; var G: Int; var B: Int;
+        var pixel: Int
+
+        val MAX_SIZE = 256
+        val MAX_VALUE_DBL = 255.0
+        val MAX_VALUE_INT = 255
+        val REVERSE = 1.0
+
+        val gammaR = arrayOfNulls<Int>(MAX_SIZE)
+        val gammaG = arrayOfNulls<Int>(MAX_SIZE)
+        val gammaB = arrayOfNulls<Int>(MAX_SIZE)
+
+        for (i in 0 until MAX_SIZE){
+            gammaR[i] = Math.min(MAX_VALUE_INT, (MAX_VALUE_DBL * Math.pow(i / MAX_VALUE_DBL, REVERSE / red) + 0.5).toInt())
+            gammaG[i] = Math.min(MAX_VALUE_INT, (MAX_VALUE_DBL * Math.pow(i / MAX_VALUE_DBL, REVERSE / green) + 0.5).toInt())
+            gammaB[i] = Math.min(MAX_VALUE_INT, (MAX_VALUE_DBL * Math.pow(i / MAX_VALUE_DBL, REVERSE / blue) + 0.5).toInt())
+        }
+
+        for(y in 0 until height){
+            for(x in 0 until width){
+                pixel = src.get(x, y)
+                A = Color.alpha(pixel)
+                R = gammaR[Color.red(pixel)]!!
+                G = gammaR[Color.green(pixel)]!!
+                B = gammaR[Color.blue(pixel)]!!
+                bmpOut.setPixel(x,y,Color.argb(A, R, G, B))
+            }
+        }
+
+        return bmpOut
+
+    }
+    fun colorFilter(src: Bitmap, red: Double, green: Double, blue: Double): Bitmap{
+        val width = src.width
+        val height = src.height
+
+        val bmpOut = Bitmap.createBitmap(width, height, src.config)
+        var A: Int; var R: Int; var G: Int; var B: Int;
+        var pixel: Int
+
+        for(y in 0 until height){
+            for(x in 0 until width){
+                pixel = src.get(x, y)
+                A = Color.alpha(pixel)
+                R = (Color.red(pixel) * red).toInt()
+                G = (Color.green(pixel) * green).toInt()
+                B = (Color.blue(pixel) * blue).toInt()
+
+                bmpOut.setPixel(x,y,Color.argb(A, R, G, B))
+            }
+        }
+
+        return bmpOut
+    }
     fun applyHueFilter(source: Bitmap, level: Int): Bitmap? {
         // get image size
         val width = source.width
@@ -364,7 +507,7 @@ class EdicionActivity:Activity() {
                 Color.colorToHSV(pixels[index], HSV)
                 // increase Saturation level
                 HSV[0] *= (level).toFloat()
-                HSV[0] = Math.max((0).toFloat(), Math.min(HSV[0],(360.0).toFloat()))
+                HSV[0] = Math.max((0).toFloat(), Math.min(HSV[0], (360.0).toFloat()))
                 // take color back
                 pixels[index] = pixels[index] or Color.HSVToColor(HSV)
             }
@@ -405,7 +548,7 @@ class EdicionActivity:Activity() {
         // Retorna el bitmap final
         return bmOut
     }
-    fun espejo(src:Bitmap): Bitmap{
+    fun espejo(src: Bitmap): Bitmap{
         val bmOut = Bitmap.createBitmap(src.width, src.height, src.config)
         var pixelColor: Int
         //Tamaño de la imagen
@@ -415,7 +558,7 @@ class EdicionActivity:Activity() {
         for (y in 0 until height) {
             for (x in 0 until width) {
                 //Obtiene un pixel
-                pixelColor = src.getPixel(width-x-1, y)
+                pixelColor = src.getPixel(width - x - 1, y)
                 // Ingresa el pixel invertido en el bitmap final
                 bmOut.setPixel(x, y, Color.argb(pixelColor.alpha, pixelColor.red, pixelColor.green, pixelColor.blue))
             }
@@ -438,9 +581,9 @@ class EdicionActivity:Activity() {
                 el = (y + 20.0 * Math.sin(2.0 * Math.PI * x / 200.0)).toInt()
                 //k = (x + 20.0 * Math.sin(2.0 * Math.PI * y / 200.0)).toInt()
                 if (el<height && el>0)
-                bmOut.setPixel(x, el ,argb(pixel.alpha, pixel.red, pixel.green, pixel.blue))
+                bmOut.setPixel(x, el, argb(pixel.alpha, pixel.red, pixel.green, pixel.blue))
                 else
-                    bmOut.setPixel(x, y ,argb(pixel.alpha, pixel.red, pixel.green, pixel.blue))
+                    bmOut.setPixel(x, y, argb(pixel.alpha, pixel.red, pixel.green, pixel.blue))
                /* if (k<width && k>0)
                     bmOut.setPixel(k, y , Color.argb(pixel.alpha, pixel.red, pixel.green, pixel.blue))
                 else
@@ -449,7 +592,7 @@ class EdicionActivity:Activity() {
         }
         return bmOut
     }
-    fun sharpen(src: Bitmap, weight:Double): Bitmap? {
+    fun sharpen(src: Bitmap, weight: Double): Bitmap? {
         val SharpConfig = arrayOf(doubleArrayOf(0.0, -2.0, 0.0), doubleArrayOf(-2.0, weight, -2.0), doubleArrayOf(0.0, -2.0, 0.0))
         val convMatrix = ConvolutionMatrix(3)
         convMatrix.applyConfig(SharpConfig)
